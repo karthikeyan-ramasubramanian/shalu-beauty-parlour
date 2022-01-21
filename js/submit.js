5bc41b1005fb27cf5622e133109adfdd6a8c4b67
karthikeyan R M <karthiramesh56@gmail.com>
2022-01-21 14:23:00 +0530

    Co-authored-by: yogeshkb-yoyo <yogeshkb-yoyo@users.noreply.github.com> Co-authored-by: Nithish kumar <nithish-27@users.noreply.github.com>

--- js/submit.js
+++
@@ -1,8 +0,0 @@
-function sendmessage(){
-  Name : document.getElementsById("name").value;
-  Email : document.getElementById("email").value;
-  Phonenumber : document.getElementById("phone number").value;
-  Message : document.getElementById("message").value;
-  console.log(Name);
-  $.get("https://api.telegram.org/bot"+token+"/sendMessage?text="+message+"&chat_id="+chat_id);
-  };
